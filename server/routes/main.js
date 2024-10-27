ee82e63cf5e20880b816bbc6ec2c3be875d6f7d2
Maria P <mariagpirela@gmail.com>
2024-10-27 14:53:37 -0500

    Views created
    Views folder created to manage views files and layouts.
    Main.ejs and the index.ejs files created.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('', async (req, res) => {
+router.get('', (req, res) => {
 
     try {
         res.render('index', {});
