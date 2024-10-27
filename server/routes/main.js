56a41998190c4832ea5df67a56e143577c76d34e
Maria P <mariagpirela@gmail.com>
2024-10-27 15:46:39 -0500

    Get and Post method for login
    The get and post method was added for the login user.
    Login form created.

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Question = require('../models/Question');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const aLayout = '../views/layouts/auth';
+const jwtSecret = process.env.JWT_SECRET;
 
+// GET Home Page
 router.get('', (req, res) => {
 
     try {
@@ -15,5 +22,49 @@ router.get('', (req, res) => {
         console.log(error);
     }
 });
+
+// GET Login
+router.get('/auth', async (req, res) => {
+  
+    try {
+        const locals = {
+            title: "The Q Forum",
+            description: "User Page For The Q Forum."
+        }
+
+        res.render('auth/login', {
+          locals,
+          layout: aLayout });
+
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+// POST login
+router.post('/auth', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      
+      const user = await User.findOne( { username } );
+  
+      if(!user) {
+        return res.status(401).send('Invalid credentials');
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if(!isPasswordValid) {
+        return res.status(401).send('Invalid credentials');
+      }
+  
+      const token = jwt.sign({ userId: user._id}, jwtSecret );
+      res.cookie('token', token, { httpOnly: true });
+      res.redirect('/');
+  
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 module.exports = router;
